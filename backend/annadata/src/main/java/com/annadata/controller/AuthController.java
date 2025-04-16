17c6c4a3e4187b7a75fffeec472c181b766979a1
amey1302 <ameykulkarni1302@gmail.com>
2025-04-17 00:23:53 +0530

    Added Register endpoint in authcontrolle and removed code redudancy

--- backend/annadata/src/main/java/com/annadata/controller/AuthController.java
+++ backend/annadata/src/main/java/com/annadata/controller/AuthController.java
@@ -1,16 +1,17 @@
 package com.annadata.controller;
 
+import com.annadata.dto.RegisterRequestDTO;
 import com.annadata.entity.Login;
+import com.annadata.entity.User;
+import com.annadata.service.AuthService;
 import com.annadata.serviceImpl.AuthServiceImpl;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,29 +24,38 @@ import java.util.Collections;
 public class AuthController {
 
     @Autowired
-    private AuthServiceImpl service;
+    private AuthService service;
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody Login loginRequest, HttpServletRequest httpServletRequest){
-        System.out.println("login called");
-        System.out.println(loginRequest.getEmail() + " : " + loginRequest.getPassword());
+    public ResponseEntity<String> login(@RequestBody Login loginRequest, HttpServletRequest request) {
+        try{
+            boolean isAuthenticated = service.authenticateUser(loginRequest);
+            if (isAuthenticated) {
+                createSessionWithSecurityContext(request, loginRequest.getEmail());
+                return new ResponseEntity<>("Login Successful", HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>("Invalid Credentials", HttpStatus.UNAUTHORIZED);
+            }
+        }catch (IllegalArgumentException exception){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
+        }
 
-        boolean isAuthenticated = service.authenticateUser(loginRequest);
-        if(isAuthenticated){
-            Authentication auth = new UsernamePasswordAuthenticationToken(
-                    loginRequest.getEmail(), null, Collections.emptyList());
-            SecurityContextHolder.getContext().setAuthentication(auth);
+    }
 
-            HttpSession session = httpServletRequest.getSession(true);
-            session.setAttribute(
-                    HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY,
-                    SecurityContextHolder.getContext()
-            );
-            System.out.println(session.getAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY));
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody RegisterRequestDTO request, HttpServletRequest requestContext) {
+        try {
+            if (service.userExists(request.getEmail()) != null) {
+                return ResponseEntity.status(HttpStatus.CONFLICT).body("User already exists");
+            }
 
-            return new ResponseEntity<>("Login Successful", HttpStatus.OK);
-        }else{
-            return new ResponseEntity<>("Invalid Credentials", HttpStatus.UNAUTHORIZED);
+            User user = service.registerUser(request);
+            createSessionWithSecurityContext(requestContext, user.getEmail());
+            return ResponseEntity.status(HttpStatus.CREATED).body("Registration successful");
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Something went wrong");
         }
     }
 
@@ -56,7 +66,17 @@ public class AuthController {
             session.invalidate();
         }
         SecurityContextHolder.clearContext();
-        return ResponseEntity.ok("Logged out successfully");
+        return ResponseEntity.status(HttpStatus.OK).body("Logged out successfully");
     }
 
+    private void createSessionWithSecurityContext(HttpServletRequest request, String email) {
+        Authentication auth = new UsernamePasswordAuthenticationToken(email, null, Collections.emptyList());
+        SecurityContextHolder.getContext().setAuthentication(auth);
+
+        HttpSession session = request.getSession(true);
+        session.setAttribute(
+                HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY,
+                SecurityContextHolder.getContext()
+        );
+    }
 }
--- backend/annadata/src/main/java/com/annadata/service/DonationService.java
+++ backend/annadata/src/main/java/com/annadata/service/DonationService.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public interface DonationService {
 
-     Donation createDonation(Donation donation);
+     DonationDTO createDonation(Donation donation);
 
      List<DonationDTO> getAllDonations();
 
