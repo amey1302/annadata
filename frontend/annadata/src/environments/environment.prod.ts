2867dacb65e3ac2ca4c758ec9be0a20f2fbf2225
amey1302 <ameykulkarni1302@gmail.com>
2025-04-21 19:46:49 +0530

    updated env files

--- frontend/annadata/src/environments/environment.prod.ts
+++ frontend/annadata/src/environments/environment.prod.ts
@@ -1,4 +1,9 @@
+// export const environment = {
+//     production: true,
+//     apiUrl: '/api/v1'
+//   };
 export const environment = {
     production: true,
-    apiUrl: '/api/v1'
+    apiUrl: '/api/v1'  // This uses the Netlify proxy
   };
+  
