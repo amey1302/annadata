bfe04d916b1bc64d8b32fcfc2c71c6aab20bc2e3
amey1302 <ameykulkarni1302@gmail.com>
2025-04-21 20:14:01 +0530

    updated to old env files

--- frontend/annadata/src/environments/environment.development.ts
+++ frontend/annadata/src/environments/environment.development.ts
@@ -1,7 +1,3 @@
-// // export const environment = {
-// //     api_url:'http://13.203.215.199:8080/food-donation/api/v1'
-// // };
 export const environment = {
-    production: true,
-    api_url: '/api/v1'  // This uses the Netlify proxy
-  };
+    api_url:'http://13.203.215.199:8080/food-donation/api/v1'
+};
--- frontend/annadata/src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-// export const environment = {
-//     production: true,
-//     apiUrl: '/api/v1'
-//   };
-export const environment = {
-    production: true,
-    api_url: '/api/v1'  // This uses the Netlify proxy
-  };
-  
