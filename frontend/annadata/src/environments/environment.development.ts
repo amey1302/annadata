1705ea4019e4350a874899b5540673963e18af65
amey1302 <ameykulkarni1302@gmail.com>
2025-04-22 22:59:19 +0530

    updated env files for railway app

--- frontend/annadata/src/environments/environment.development.ts
+++ frontend/annadata/src/environments/environment.development.ts
@@ -15,7 +15,7 @@ export const environment = {
     //api_url:'http://localhost:8080/food-donation/api/v1'
     // api_url:'https://annadata-4.onrender.com/food-donation/api/v1'
     // https://annadata-4.onrender.com/food-donation/api/v1/donations
-    api_url:'https://annadata-production-e98f.up.railway.app:8080/food-donation/api/v1'
+    api_url:'https://annadata-production-e98f.up.railway.app/food-donation/api/v1'
 
 
 };
