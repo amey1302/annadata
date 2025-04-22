2fe2cfea02248dc2f96e5272719e75c39933e8e1
amey1302 <ameykulkarni1302@gmail.com>
2025-04-22 22:52:53 +0530

    updated env files for railway app

--- frontend/annadata/src/environments/environment.ts
+++ frontend/annadata/src/environments/environment.ts
@@ -1,7 +1,13 @@
 // export const environment = {
 //     api_url:'http://13.203.215.199:8080/food-donation/api/v1'
 // };
+// export const environment = {
+//     // api_url:'http://13.203.215.199:8080/food-donation/api/v1'
+//     api_url:'https://annadata-4.onrender.com/food-donation/api/v1'
+// };
+
 export const environment = {
     // api_url:'http://13.203.215.199:8080/food-donation/api/v1'
-    api_url:'https://annadata-4.onrender.com/food-donation/api/v1'
+    //  api_url:'https://annadata-4.onrender.com/food-donation/api/v1'
+    api_url:'https://annadata-production-e98f.up.railway.app:8080/food-donation/api/v1'
 };
