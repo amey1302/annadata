7ee71c3fde1cb019caf2f31473744520c085164e
amey1302 <ameykulkarni1302@gmail.com>
2025-04-22 10:20:31 +0530

    updated the env files

--- frontend/annadata/src/environments/environment.development.ts
+++ frontend/annadata/src/environments/environment.development.ts
@@ -1,10 +1,14 @@
-export const environment = {
-// <<<<<<< Updated upstream
-    api_url:'http://13.203.215.199:8080/food-donation/api/v1'
-// =======
-    // api_url:'http://localhost:8080/food-donation/api/v1'
-    // api_url:'http://13.203.215.199:8080/food-donation/api/v1'
+// export const environment = {
 
-// >>>>>>> Stashed changes
+//     api_url:'http://13.203.215.199:8080/food-donation/api/v1'
+
+// };
+
+export const environment = {
+
+    api_url:'https://annadata-4.onrender.com/food-donation/api/v1'
+    // https://annadata-4.onrender.com/food-donation/api/v1/donations
+
 };
+
 
--- frontend/annadata/src/environments/environment.ts
+++ frontend/annadata/src/environments/environment.ts
@@ -1,3 +1,7 @@
+// export const environment = {
+//     api_url:'http://13.203.215.199:8080/food-donation/api/v1'
+// };
 export const environment = {
-    api_url:'http://13.203.215.199:8080/food-donation/api/v1'
+    // api_url:'http://13.203.215.199:8080/food-donation/api/v1'
+    api_url:'https://annadata-4.onrender.com/food-donation/api/v1'
 };
