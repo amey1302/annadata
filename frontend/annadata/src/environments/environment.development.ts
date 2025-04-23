c728e57ad80da75ed0a20323a8a7336a29318a50
amey1302 <ameykulkarni1302@gmail.com>
2025-04-23 20:05:45 +0530

    updated for render env and updated dist files

--- frontend/annadata/src/environments/environment.development.ts
+++ frontend/annadata/src/environments/environment.development.ts
@@ -1,23 +1,7 @@
-// export const environment = {
-
-//     api_url:'http://13.203.215.199:8080/food-donation/api/v1'
-
-// };
-
-// export const environment = {
-//     //api_url:'http://localhost:8080/food-donation/api/v1'
-//     api_url:'https://annadata-4.onrender.com/food-donation/api/v1'
-//     // https://annadata-4.onrender.com/food-donation/api/v1/donations
-
-// };
-
 export const environment = {
     //api_url:'http://localhost:8080/food-donation/api/v1'
-    api_url:'https://annadata-production-e98f.up.railway.app/food-donation/api/v1'
-    // https://annadata-4.onrender.com/food-donation/api/v1/donations
     // api_url:'https://annadata-production-e98f.up.railway.app/food-donation/api/v1'
-
-
+    api_url:'https://annadata-4.onrender.com/food-donation/api/v1'
 };
 
 
--- frontend/annadata/src/environments/environment.ts
+++ frontend/annadata/src/environments/environment.ts
@@ -1,11 +1,3 @@
-// export const environment = {
-//     api_url:'http://13.203.215.199:8080/food-donation/api/v1'
-// };
-// export const environment = {
-//     // api_url:'http://13.203.215.199:8080/food-donation/api/v1'
-//     api_url:'https://annadata-4.onrender.com/food-donation/api/v1'
-// };
-
 export const environment = {
     // api_url:'http://13.203.215.199:8080/food-donation/api/v1'
      api_url:'https://annadata-4.onrender.com/food-donation/api/v1'
